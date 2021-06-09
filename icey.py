d0751aa745fe5eb7dee06737c9f02768a8735254
Dotsbeauty <drishti0353@gmail.com>
2021-06-09 21:51:33 +0530

    added nose

--- icey.py
+++ icey.py
@@ -14,13 +14,14 @@ body = c.create_oval(35, 20, 365, 360, outline="black", fill=c.body_color)
 
 eye_left = c.create_oval(140,150,170,180,outline='black' , fill="black")
 eye_right = c.create_oval(240,150,270,180,outline='black' , fill="black")
+# eye_angry = c.create_
 
 nose = c.create_oval(200-20,210,200+20,225, outline="black", fill="black")
 nose_curve = c.create_line(150,200,200,180,250,200, smooth=1 , width=1 ,state=NORMAL)
 
-mouth_normal = c.create_line(180,250,200,260,220,250, smooth=1 , width=2 ,state=NORMAL)
+mouth_normal = c.create_line(180,250,200,260,220,250, smooth=1 , width=2 ,state=HIDDEN)
 mouth_happy = c.create_line(170,250,200,282,230,250,smooth=1 , width=2 , state=HIDDEN)
-mouth_sad = c.create_line(170,250,200,230,230,250,smooth=1 , width=2 , state=HIDDEN)
+mouth_sad = c.create_line(170,250,200,230,230,250,smooth=1 , width=2 , state=NORMAL)
 
 cheek_left = c.create_oval(70,180,120,230,outline='pink' , fill='pink',state=HIDDEN)
 cheek_right = c.create_oval(280,180,330,230,outline='lightpink' , fill='pink',state=HIDDEN)
